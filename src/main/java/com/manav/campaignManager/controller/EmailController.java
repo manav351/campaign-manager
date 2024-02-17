5c2d09eb119328f4654954c48d508ca8cc9b302d
Manav Mittal <44033525+manav351@users.noreply.github.com>
2024-02-17 21:27:47 +0530

    Adds Email Controller

---
+++ src/main/java/com/manav/campaignManager/controller/EmailController.java
@@ -0,0 +1,61 @@
+package com.manav.campaignManager.controller;
+
+import com.manav.campaignManager.entity.Email;
+import com.manav.campaignManager.entity.GenericResponse;
+import com.manav.campaignManager.entity.Status;
+import com.manav.campaignManager.service.EmailService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/email")
+public class EmailController {
+    
+    final EmailService serviceObj;
+    
+    public EmailController(final EmailService serviceObj) {
+        this.serviceObj = serviceObj;
+    }
+    
+    @GetMapping("/all")
+    public ResponseEntity<GenericResponse<Email>> getAllEmailNotification(){
+        return new ResponseEntity<>(
+                GenericResponse.<Email>builder()
+                        .status(Status.builder().status(true).message("Operation Successful").error("").build())
+                        .data(serviceObj.findAllNotifications())
+                        .build(),
+                HttpStatus.OK
+        );
+    }
+    
+    @GetMapping({"", "/"})
+    public ResponseEntity<GenericResponse<Email>> getEmailNotification(@RequestParam Integer notification_id){
+        return new ResponseEntity<>(
+                GenericResponse.<Email>builder()
+                        .status(Status.builder().status(true).message("Operation Successful").error("").build())
+                        .data(serviceObj.findNotification(notification_id))
+                        .build(),
+                HttpStatus.OK
+        );
+    }
+    
+    @PostMapping("/send")
+    public ResponseEntity<GenericResponse<Email>> sendEmail(@RequestBody Email email){
+        Email sentEmail = serviceObj.sendEmail(email);
+        GenericResponse<Email> response = GenericResponse.<Email>builder()
+                .status(
+                        Status.builder()
+                                .status(true)
+                                .message("Operation Successful")
+                                .error("")
+                                .build()
+                )
+                .data(List.of(sentEmail))
+                .build();
+        
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
